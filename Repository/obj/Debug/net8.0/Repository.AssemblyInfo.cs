9c3c9728fcf01ad0de5115b47b1ad82ab0837688
minh nhat <nhat892003@gmail.com>
2024-06-10 22:04:33 +0700

    new repo

--- Repository/obj/Debug/net8.0/Repository.AssemblyInfo.cs
+++ Repository/obj/Debug/net8.0/Repository.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Repository")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f1a570fa0c28cc89798c151cf7ed4e8fd0a0f2dc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6881e4e9749352aa45bb33a12d9294d84f11e4f4")]
 [assembly: System.Reflection.AssemblyProductAttribute("Repository")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Repository")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
