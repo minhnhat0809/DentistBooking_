24a980bcac0ec85704cdc2dd435588519f0e7855
phamduchuy <huypd284@gmail.com>
2024-07-01 10:21:47 +0700

    add layout prescritiotion, fix repo

---
+++ DentistBooking/Pages/Dentis/PrescriptionManagement/Create.cshtml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using BusinessObject;
+using DataAccess;
+
+namespace DentistBooking.Pages.Dentis.PrescriptionManagement
+{
+    public class CreateModel : PageModel
+    {
+        private readonly DataAccess.BookingDentistDbContext _context;
+
+        public CreateModel(DataAccess.BookingDentistDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult OnGet()
+        {
+        ViewData["AppointmentId"] = new SelectList(_context.Appointments, "AppointmentId", "AppointmentId");
+            return Page();
+        }
+
+        [BindProperty]
+        public Prescription Prescription { get; set; } = default!;
+
+        // To protect from overposting attacks, see https://aka.ms/RazorPagesCRUD
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.Prescriptions.Add(Prescription);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
--- Service/Impl/PrescriptionService.cs
+++ Service/Impl/PrescriptionService.cs
@@ -1,12 +1,124 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using BusinessObject;
+using Repository;
+using Service.Exeption;
 
 namespace Service.Impl
 {
     public class PrescriptionService : IPrescriptionService
     {
+        private readonly IPrescriptionrepo _preScription;
+
+        public PrescriptionService(IPrescriptionrepo prescription)
+        {
+            _preScription = prescription ?? throw new ArgumentNullException(nameof(prescription));
+        }
+        public void CreatePrescription(Prescription prescription)
+        {
+            if (prescription == null)
+            {
+                throw new ArgumentNullException(nameof(prescription), "Prescription cannot be null.");
+            }
+
+            try
+            {
+                var existingPrescription = _preScription.GetById(prescription.PrescriptionId);
+                if (existingPrescription != null)
+                {
+                    throw new InvalidOperationException($"Medicine with ID {prescription.PrescriptionId} already exists.");
+                }
+
+                _preScription.CreatePrescription(prescription);
+            }
+            catch (Exception ex)
+            {
+                // Log exception
+                throw new ExceptionHandler.ServiceException("An error occurred while creating the prescription.", ex);
+            }
+        }
+
+        public void DeletePrescription(int id)
+        {
+            if (id <= 0)
+            {
+                throw new ArgumentException("Invalid prescription ID.", nameof(id));
+            }
+
+            try
+            {
+                var existingMedicine = _preScription.GetById(id);
+                if (existingMedicine == null)
+                {
+                    throw new ExceptionHandler.NotFoundException($"Prescription with ID {id} not found.");
+                }
+
+                _preScription.DeletePrescription(id);
+            }
+            catch (Exception ex)
+            {
+                // Log exception
+                throw new ExceptionHandler.ServiceException("An error occurred while deleting the prescription.", ex);
+            }
+        }
+
+        public Prescription GetById(int id)
+        {
+            if (id <= 0)
+            {
+                throw new ArgumentException("Invalid prescription ID.", nameof(id));
+            }
+
+            try
+            {
+                var model = _preScription.GetById(id);
+                if (model == null)
+                {
+                    throw new ExceptionHandler.NotFoundException($"Prescription with ID {id} not found.");
+                }
+
+                return model;
+            }
+            catch (Exception ex)
+            {
+                // Log exception
+                throw new ExceptionHandler.ServiceException("An error occurred while retrieving the Prescription.", ex);
+            }
+        }
+
+        public List<Prescription> GetPrescriptions()
+        {
+            try
+            {
+                return _preScription.GetPrescriptions();
+            }
+            catch (Exception ex)
+            {
+                // Log exception
+                throw new ExceptionHandler.ServiceException("An error occurred while retrieving Prescription.", ex);
+            }
+        }
+
+        public void UpdatePrescription(Prescription prescription)
+        {
+            if (prescription == null)
+            {
+                throw new ArgumentNullException(nameof(prescription), "prescription cannot be null.");
+            }
+
+            try
+            {
+                var existingMedicine = _preScription.GetById(prescription.PrescriptionId);
+                if (existingMedicine == null)
+                {
+                    throw new ExceptionHandler.NotFoundException($"prescription with ID {prescription.PrescriptionId} not found.");
+                }
+
+                _preScription.UpdatePrescription(prescription);
+            }
+            catch (Exception ex)
+            {
+                // Log exception
+                throw new ExceptionHandler.ServiceException("An error occurred while updating the prescription.", ex);
+            }
+        }
     }
 }
