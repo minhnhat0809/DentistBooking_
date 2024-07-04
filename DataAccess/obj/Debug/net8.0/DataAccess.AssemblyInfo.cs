1ee8321a6390f6e33d6b900f6c613911fce28d8f
Nguyen Vu Truong Huy <nguyenvutruonghuy112@gmail.com>
2024-07-04 15:10:15 +0700

    commit

--- DataAccess/obj/Debug/net8.0/DataAccess.AssemblyInfo.cs
+++ DataAccess/obj/Debug/net8.0/DataAccess.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DataAccess")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+57da37334d80b6657e6dce5b9beae61c42d4c836")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e0cec62328de1af805e023eff565c1ba451f1fd0")]
 [assembly: System.Reflection.AssemblyProductAttribute("DataAccess")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DataAccess")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
