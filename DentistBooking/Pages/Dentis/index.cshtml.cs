5e3ade619ae1291c39b9d6d96a7b771eefcd5da7
Nguyen Vu Truong Huy <nguyenvutruonghuy112@gmail.com>
2024-07-05 23:36:50 +0700

    PrescriptionMedicines

---
+++ DentistBooking/Pages/Dentis/index.cshtml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using BusinessObject;
+using DataAccess;
+
+namespace DentistBooking.Pages.Dentis
+{
+    public class indexModel : PageModel
+    {
+        private readonly DataAccess.BookingDentistDbContext _context;
+
+        public indexModel(DataAccess.BookingDentistDbContext context)
+        {
+            _context = context;
+        }
+
+        public IList<PrescriptionMedicine> PrescriptionMedicine { get;set; } = default!;
+
+        public async Task OnGetAsync()
+        {
+            PrescriptionMedicine = await _context.PrescriptionMedicines
+                .Include(p => p.Medicine)
+                .Include(p => p.Prescription).ToListAsync();
+        }
+    }
+}
---
+++ Service/Impl/PrescriptionMedicinesService.cs
@@ -0,0 +1,130 @@
+﻿using BusinessObject;
+using Repository.Impl;
+using Service.Exeption;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Service.Impl
+{
+    public interface IPrescriptionMedicinesService
+    {
+        public Task<List<PrescriptionMedicine>> GetAllPrescriptionMedicines();
+        public Task<List<PrescriptionMedicine>> GetAllPrescriptionMedicinesByPrescriptionId( int preId);
+        public Task<PrescriptionMedicine> GetById(int? id);
+        public void AddPrescriptionMedicine(PrescriptionMedicine prescriptionMedicine);
+        public void DeletePrescriptionMedicine(PrescriptionMedicine prescriptionMedicine);
+        public void UpdatePrescriptionMedicine(PrescriptionMedicine prescriptionMedicine);
+    }
+    public class PrescriptionMedicinesService : IPrescriptionMedicinesService
+    {
+        private readonly IPrescriptionMedicineRepo _prescriptionMedicineRepo;
+        public PrescriptionMedicinesService(IPrescriptionMedicineRepo prescriptionMedicineRepo)
+        {
+            _prescriptionMedicineRepo = prescriptionMedicineRepo;
+        }
+        public void UpdatePrescriptionMedicine(PrescriptionMedicine prescriptionMedicine)
+        {
+            if (prescriptionMedicine == null)
+            {
+                throw new ArgumentNullException(nameof(prescriptionMedicine), "prescriptionMedicine cannot be null.");
+            }
+
+            try
+            {
+                _prescriptionMedicineRepo.UpdatePrescriptionMedicine(prescriptionMedicine);
+
+
+            }
+            catch (Exception ex)
+            {
+                // Log exception
+                throw new ExceptionHandler.ServiceException("An error occurred while updating.", ex);
+            }
+
+        }
+        public void AddPrescriptionMedicine(PrescriptionMedicine prescriptionMedicine)
+        {
+            if (prescriptionMedicine == null)
+            {
+                throw new ArgumentNullException(nameof(prescriptionMedicine), "prescriptionMedicine cannot be null.");
+            }
+
+            try
+            {
+                _prescriptionMedicineRepo.AddPrescriptionMedicine(prescriptionMedicine);
+
+            }
+            catch (Exception ex)
+            {
+                // Log exception
+                throw new ExceptionHandler.ServiceException("An error occurred while creating the prescription.", ex);
+            }
+        }
+
+        public async Task<List<PrescriptionMedicine>> GetAllPrescriptionMedicinesByPrescriptionId(int preId)
+        {
+            
+            try
+            {
+                var models = await _prescriptionMedicineRepo.GetAllPrescriptionMedicinesByPrescriptionId(preId);
+                return models;
+            }
+            catch (Exception ex)
+            {
+                // Log exception
+                throw new ExceptionHandler.ServiceException("An error occurred while retrieving.", ex);
+            }
+        }
+
+        public async Task<PrescriptionMedicine> GetById(int? id)
+        {
+
+            try
+            {
+                var model = await _prescriptionMedicineRepo.GetById(id);
+                return model;
+            }
+            catch (Exception ex)
+            {
+                // Log exception
+                throw new ExceptionHandler.ServiceException("An error occurred while retrieving.", ex);
+            }
+        }
+
+        public void DeletePrescriptionMedicine(PrescriptionMedicine prescriptionMedicine)
+        {
+            if (prescriptionMedicine == null)
+            {
+                throw new ArgumentNullException(nameof(prescriptionMedicine), "prescriptionMedicine cannot be null.");
+            }
+
+            try
+            {
+                _prescriptionMedicineRepo.DeletePrescriptionMedicine(prescriptionMedicine);
+
+            }
+            catch (Exception ex)
+            {
+                // Log exception
+                throw new ExceptionHandler.ServiceException("An error occurred while creating the prescription.", ex);
+            }
+        }
+
+        public async Task<List<PrescriptionMedicine>> GetAllPrescriptionMedicines()
+        {
+            try
+            {
+                var models = await _prescriptionMedicineRepo.GetAllPrescriptionMedicines();
+                return models;
+            }
+            catch (Exception ex)
+            {
+                // Log exception
+                throw new ExceptionHandler.ServiceException("An error occurred while retrieving.", ex);
+            }
+        }
+    }
+}
