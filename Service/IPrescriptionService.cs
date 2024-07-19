af6f39c4e54e1114be6d5d894fe1b3873f8764c4
Nguyen Vu Truong Huy <nguyenvutruonghuy112@gmail.com>
2024-07-19 14:05:19 +0700

    IService : Async

--- Service/IPrescriptionService.cs
+++ Service/IPrescriptionService.cs
@@ -5,11 +5,11 @@ namespace Service
 {
     public interface IPrescriptionService
     {
-        public Task<List<PrescriptionDto>> GetPrescriptions();
-        public Task<PrescriptionDto> GetById(int id);
-        public void CreatePrescription(PrescriptionDto prescription);
-        public void UpdatePrescription(PrescriptionDto prescription);
-        public void DeletePrescription(int id);
+        Task<List<PrescriptionDto>> GetPrescriptions();
+        Task<PrescriptionDto> GetById(int id);
+        Task CreatePrescription(PrescriptionDto prescription);
+        Task UpdatePrescription(PrescriptionDto prescription);
+        Task DeletePrescription(int id);
         Task<PrescriptionDto> GetByIdWithMedicinesAsync(int id);
 
         Task<List<PrescriptionDto>> GetAllPrescriptionByCustomer(int customerId);
--- Service/IService.cs
+++ Service/IService.cs
@@ -6,11 +6,11 @@ namespace Service
     public interface IService
     {
        
-        public Task<ServiceDto> GetServiceByID(int id);
-        public Task<List<ServiceDto>> GetAllServices();
-        public void DeleteService(ServiceDto service);
-        public void CreateService(ServiceDto service);
-        public void UpdateService(ServiceDto service);
+        Task<ServiceDto> GetServiceByID(int id);
+        Task<List<ServiceDto>> GetAllServices();
+        Task DeleteService(ServiceDto service);
+        Task CreateService(ServiceDto service);
+        Task UpdateService(ServiceDto service);
         Task<List<ServiceDto>> GetAllServicesForCustomer(int serviceId);
         Task<IEnumerable<ServiceDto>> GetAllServicesAsync();
         Task<IEnumerable<ServiceDto>> GetServicesByDentistSlotAsync(int dentistSlotId);
