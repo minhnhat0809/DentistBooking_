e3f7a4306cd0e2b00db63a56922cce147b892df5
Nguyen Vu Truong Huy <nguyenvutruonghuy112@gmail.com>
2024-07-21 22:18:32 +0700

    commit

---
+++ DentistBooking/Pages/DentistPage/Appointments/Details.cshtml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using BusinessObject;
+using Service;
+using BusinessObject.DTO;
+
+namespace DentistBooking.Pages.DentistPage.Appointments
+{
+    public class DetailsModel : PageModel
+    {
+        private readonly IAppointmentService _appointmentService;
+
+        public DetailsModel(IAppointmentService appointmentService)
+        {
+            _appointmentService = appointmentService;
+        }
+
+        public AppointmentDto Appointment { get; set; } = default!;
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var appointment = await _appointmentService.GetAppointmentByID(id.Value);
+            if (appointment == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                Appointment = appointment;
+            }
+            return Page();
+        }
+    }
+}
