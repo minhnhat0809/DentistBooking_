f074079841082e7db2a6f6d587943564d5e0914a
Nguyen Vu Truong Huy <nguyenvutruonghuy112@gmail.com>
2024-07-25 04:09:14 +0700

    Customer Page: Service Paging + Signal R setting DentistSlot

--- DentistBooking/Pages/CustomerPage/ViewPrescriptions.cshtml.cs
+++ DentistBooking/Pages/CustomerPage/ViewPrescriptions.cshtml.cs
@@ -1,23 +1,59 @@
 using BusinessObject;
 using BusinessObject.DTO;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore.Metadata.Internal;
 using Service;
+using Service.Impl;
+using System.Drawing.Printing;
+using X.PagedList;
 
 namespace DentistBooking.Pages.CustomerPage;
 
 public class ViewPrescriptions : PageModel
 {
     private readonly IPrescriptionService prescriptionService;
+    private readonly IUserService userService;
 
-    public ViewPrescriptions(IPrescriptionService prescriptionService)
+    public ViewPrescriptions(IPrescriptionService prescriptionService, IUserService userService)
     {
         this.prescriptionService = prescriptionService;
+        this.userService = userService;
     }
-    public IList<PrescriptionDto> Prescriptions { get; set; }
-    public async void OnGet()
+    public IPagedList<PrescriptionDto> Prescriptions { get; set; }
+
+    [BindProperty(SupportsGet = true)]
+
+    public int PageNumber { get; set; } = 1;
+
+    [BindProperty(SupportsGet = true)]
+
+    public int PageSize { get; set; } = 5;
+
+    public async Task<IActionResult> OnGet()
     {
-        Prescriptions = await prescriptionService.GetAllPrescriptionByCustomer
-            (Int32.Parse(HttpContext.Session.GetString("ID")));
+        string? email = HttpContext.Session.GetString("Email");
+        if (!string.IsNullOrEmpty(email))
+        {
+            var users = await userService.GetAllDentists();
+            if (users != null)
+            {
+                var user = users.FirstOrDefault(x => x.Email == email);
+                if (user == null)
+                {
+                    TempData["ErrorMessage"] = "No dentist found with the provided email.";
+                    return RedirectToPage("/Error");
+                }
+                var prescriptions = await prescriptionService.GetAllPrescriptionByCustomer(user.UserId);
+                Prescriptions = prescriptions.ToPagedList(PageNumber, PageSize);
+                return Page();
+            }
+            else
+            {
+                TempData["ErrorMessage"] = "No dentist found with the provided email.";
+                return RedirectToPage("/Error");
+            }
+        }
+        else return RedirectToPage("/Denied");
     }
 }
